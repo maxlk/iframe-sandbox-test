35672bea19d81c1ed0c11114ea1ec71c60c52b56
Maxim Kulikov <maxim.kulikov@vimeo.com>
2025-04-29 16:04:56 +0300

    add logic to js file

--- escape-sandbox.js
+++ escape-sandbox.js
@@ -1,7 +1,26 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <script src="./escape-sandbox.js"></script>
-  </head>
-  <body></body>
-</html>
+let cookie = document.cookie;
+let storage = { ...localStorage };
+document.write(`<h1>Cookie:</h1><pre>${cookie}</pre>`);
+document.write(`<h1>LocalStorage:</h1><pre>${storage}</pre>`);
+
+let preferencesURL = "https://api.vimeo.com/me/preferences?fields=tves,dai";
+async function fetchPreferences() {
+  const response = await fetch(preferencesURL);
+  if (responce.ok) {
+    const data = await response.json();
+    document.getElemetsByClassName(
+      "HTML"
+    )[0].innerHTML += `<h1>User Preferences:</h1><pre>${JSON.stringify(
+      data,
+      null,
+      2
+    )}</pre>`;
+  } else {
+    const error = await response.text();
+    document.getElemetsByClassName(
+      "HTML"
+    )[0].innerHTML += `<h1>Error fetching preferences</h1><pre>${response.status} ${response.statusText} ${error}</pre>`;
+  }
+}
+
+fetchPreferences();
